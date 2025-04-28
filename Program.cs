70d9222b1025878576e42f83038c1485496ea74d
gabriel Malaquias <168035338+gbpool@users.noreply.github.com>
2025-04-27 22:05:13 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+Console.WriteLine("digite o primeiro numero");
+decimal primeiroNumero = decimal.Parse(Console.ReadLine()!);
+
+
+Console.WriteLine("digite o segundo numero");
+decimal segundoNumero = decimal.Parse(Console.ReadLine()!);
+
+Console.WriteLine("digite a operação desejada: +, -, *, /");
+string operacao = Console.ReadLine()!;
+
+switch (operacao) {
+    case "+":
+        Console.WriteLine($"Resultado: {primeiroNumero + segundoNumero}");
+        break;
+    case "-":
+        Console.WriteLine($"Resultado: {primeiroNumero - segundoNumero}");
+        break;
+    case "*":
+        Console.WriteLine($"Resultado: {primeiroNumero * segundoNumero}");
+        break;
+    case "/":
+        if (segundoNumero != 0)
+            Console.WriteLine($"Resultado: {primeiroNumero / segundoNumero}");
+        else
+            Console.WriteLine("Divisão por zero não é permitida.");
+        break;
+    default:
+        Console.WriteLine("Operação inválida.");
+        break;
+} 
